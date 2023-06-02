36dc074c242f4cf4e15496212a0f5199a91dcd20
Oleg Zasukha <ozamericos@gmail.com>
2023-06-02 00:19:12 +0000

    Inner and Outer Joins

---
+++ problem2.sql
@@ -0,0 +1,13 @@
+
+
+        /**
+         * Problem 2: In problem2.sql, Use a RIGHT OUTER JOIN to combine the class (left side) and textbook
+         * (right side) tables using the class_title column as the join on column. This means that when no class
+         * exists for a textbook, a row with a NULL class will be in the resulting query.
+         * Hint: You should start with "SELECT * FROM class"
+         */
+
+         select * 
+         from class 
+         right join textbook
+         on class.class_title = textbook.class_title; 
